b9e9b2b1df70becd6f9e6fd8ef40d0a938f96e5d
unknown <ahmedjubapu11@gmail.com>
2024-11-27 15:58:35 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// JavaScript for smooth scrolling between sections
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
